a46eeb12a711ca92047b643a45dbbedfe0dd6fd0
aanper <mail@s3f.ru>
2022-11-15 03:20:16 +0400

    fastest sin

--- grafic_pattern.py
+++ grafic_pattern.py
@@ -51,11 +51,50 @@ def next_seance_init():
     """
     pass
 
-def get_track_point(p):
-    return (sin(p) + 0.5, sin(p))
+SIN_TABLE = [ 127, 130, 133, 136, 139, 142, 145, 148, 151, 154, 157, 160, 163,
+166, 169, 172, 175, 178, 181, 184, 186, 189, 192, 194, 197, 200, 202, 205, 207,
+209, 212, 214, 216, 218, 221, 223, 225, 227, 229, 230, 232, 234, 235, 237, 239,
+240, 241, 243, 244, 245, 246, 247, 248, 249, 250, 250, 251, 252, 252, 253, 253,
+253, 253, 253, 254, 253, 253, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248,
+247, 246, 245, 244, 243, 241, 240, 239, 237, 235, 234, 232, 230, 229, 227, 225,
+223, 221, 218, 216, 214, 212, 209, 207, 205, 202, 200, 197, 194, 192, 189, 186,
+184, 181, 178, 175, 172, 169, 166, 163, 160, 157, 154, 151, 148, 145, 142, 139,
+136, 133, 130, 127, 123, 120, 117, 114, 111, 108, 105, 102, 99, 96, 93, 90, 87,
+84, 81, 78, 75, 72, 69, 67, 64, 61, 59, 56, 53, 51, 48, 46, 44, 41, 39, 37, 35,
+32, 30, 28, 26, 24, 23, 21, 19, 18, 16, 14, 13, 12, 10, 9, 8, 7, 6, 5, 4, 3, 3,
+2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10,
+12, 13, 14, 16, 18, 19, 21, 23, 24, 26, 28, 30, 32, 35, 37, 39, 41, 44, 46, 48,
+51, 53, 56, 59, 61, 64, 67, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102,
+105, 108, 111, 114, 117, 120, 123,] 
+
+def fastest_sin(x):
+    return SIN_TABLE[int(x) % 256]
+
+MAX_DETUNE = 4
+
+class TrackParam:
+    def __init__(self):
+        self.a_x = 1
+        self.a_y = 1
+        self.detune = MAX_DETUNE/2
+        self.x = 0
+        self.y = 0
+        self.phase_x = 0
+        self.phase_y = 1.5
+        
+def get_track_point(p, param):
+    return (
+        param.a_x * (fastest_sin(255 * (p * param.detune + param.phase_x) / (2*pi)) - 127) / 127 + param.x,
+        param.a_y * (fastest_sin(255 * (p * (MAX_DETUNE - param.detune) + param.phase_y) / (2*pi)) - 127) / 127 + param.y
+    )
 
 def euclid(a, b):
     return (a[0] - b[0])**2 + (a[1] - b[1])**2
+
+def orthmin(a,b):
+    return min(abs(a[0] - b[0]), abs(a[1] - b[1]))
+def orthmax(a,b):
+    return max(abs(a[0] - b[0]), abs(a[1] - b[1]))
 
 LED_RADIUS = 2
 
@@ -91,8 +130,9 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     while True:
         for t in range(0, 100):
-            track_point = get_track_point(t * 2 * pi / 100)
+            param = TrackParam()
+            track_point = get_track_point(t * 2 * pi / 100, param)
             led_value = [
                 max(0, min(1, 2 - euclid(track_point, led_point))) * 120 for led_point in LED_POSITIONS]
             draw_scene(led_value)
-            sleep(0.02)
+            sleep(0.05)
